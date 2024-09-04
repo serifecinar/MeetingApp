acd8122d6b28bf8c3d862301903d0bb071c488e4
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-04 09:06:48 +0300

    Proje Oluşturma

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();//mvc şablonu projeye tanıtılmış oluyor 
+
 var app = builder.Build();
+
+//proje şablonları--
+//mvc
+//rest api
+//razor pages
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/abc", () => "deneme");
