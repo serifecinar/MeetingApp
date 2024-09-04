67269041f33cdcc74553a533b57dfe51baab1cc8
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-04 11:02:39 +0300

    layout

--- obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd8122d6b28bf8c3d862301903d0bb071c488e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46384007a728a4c74ae2c993b42773d48264e18f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
