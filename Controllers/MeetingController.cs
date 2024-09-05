0787045358095affe92edcfaceef0f7250d2cd27
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-05 13:40:21 +0300

    model binding

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -4,16 +4,28 @@ namespace MeetingApp.Controllers
 {
     public class MeetingController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Apply()
         {
 
             return View();
         }
+        [HttpPost]
+        public IActionResult Apply(string Name,string Phone,string Email,bool WillAttend)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Phone);
+            Console.WriteLine(Email);
+            Console.WriteLine(WillAttend);
+            return View();
+        }
+        [HttpGet]
         public IActionResult List()
         {
 
--- obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ebc876ce0daa95793540db807c36bc8aa7a7b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71bdd7ac9c92430a4cd71903ea83d750b2e03f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
