060102376fba4a412fdd8c02c54d6564ca608810
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-25 11:17:52 +0300

    meeting app son

--- obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f024224e6c46e50cc89a74b06ba6996d224ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321b11ddeac3ee9ab8727cbad68678f7517cb04b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
